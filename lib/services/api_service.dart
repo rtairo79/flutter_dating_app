21ee54b94ee999a1baba5074b2126318de96cfc1
rtairo79 <rtairo79@gmail.com>
2025-05-06 17:02:20 +0500

    update to localhost configuraton

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ApiService {
-  static const baseUrl = 'http://127.0.0.1:8000/api/';
+  static const baseUrl = 'http://192.168.8.14:8000/api/';
 
   static Future<List<dynamic>> getMusicMatches(int userId) async {
     final response = await http.get(Uri.parse('${baseUrl}users/$userId/music-matches/'));
